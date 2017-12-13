469c18da5b001b9ab477380add14f4793de244ca
liushahe <1540077031@qq.com>
2017-12-13 14:58:55 +0800

    新增安全配置文件

---
+++ src/main/java/com/work/blog/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.work.blog.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+	}
+
+}
