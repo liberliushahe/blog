0d513a09bbb2fb94c71df655ea7de7ecbf586a91
liushahe <1540077031@qq.com>
2017-12-13 17:10:45 +0800

    优化主程序

--- src/main/java/com/work/blog/BlogApplication.java
+++ src/main/java/com/work/blog/BlogApplication.java
@@ -2,7 +2,12 @@ package com.work.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 应用程序启动类
+ * @author jijiuxue
+ * <a href="https://github.com/liberliushahe/blog"></a>
+ * @creation 2017年12月13日
+ */
 @SpringBootApplication
 public class BlogApplication {
 
