978a7b68b4d9cdb761e0a7a9077ec105ef1f5c5a
liushahe <1540077031@qq.com>
2017-12-13 17:10:05 +0800

    开发用户实体类

--- src/main/java/com/work/blog/controller/UserController.java
+++ src/main/java/com/work/blog/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.work.blog.controller;
-
+/**
+ * 用户控制器类
+ * @author jijiuxue
+ * <a href="https://github.com/liberliushahe/blog"></a>
+ * @creation 2017年12月13日
+ */
 public class UserController {
 
 }
