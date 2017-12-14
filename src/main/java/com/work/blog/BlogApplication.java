782fc980f98a73e94827f1d369d83336ba9b572d
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2017-12-14 13:51:28 +0800

    优化主程序

--- src/main/java/com/work/blog/BlogApplication.java
+++ src/main/java/com/work/blog/BlogApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class BlogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
