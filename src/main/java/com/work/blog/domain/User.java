e78721ee73d2e888a62fa43630777593a63bf71a
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2017-12-14 22:27:54 +0800

    增加评论实体

--- src/main/java/com/work/blog/domain/User.java
+++ src/main/java/com/work/blog/domain/User.java
@@ -8,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Size;
-
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 /**
